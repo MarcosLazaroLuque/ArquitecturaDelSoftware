e511c6da1a546c567f30b65fc78b3c927f3108d2
Marcos Lázaro Luque <mlazarolu.inf@upsa.es>
2026-02-26 15:04:20 +0100

    practica 3

--- app_gestion_taller/admin.py
+++ app_gestion_taller/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Cliente, Coche, Servicio, CocheServicio
 
 admin.site.register(Cliente)
 admin.site.register(Coche)
 admin.site.register(Servicio)
 admin.site.register(CocheServicio)
-=======
-
-# Register your models here.
->>>>>>> 41263e665ae1dcc967eb641006b9b880fd8bc7eb
