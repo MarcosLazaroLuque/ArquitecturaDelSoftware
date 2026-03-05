87e0bb1c93774227fff403e4a226c43fd51f27c9
Marcos Lázaro Luque <mlazarolu.inf@upsa.es>
2026-03-05 12:30:13 +0100

    Update views.py

--- app_gestion_taller/views.py
+++ app_gestion_taller/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.http import JsonResponse
 from .models import Cliente
 
@@ -12,8 +11,3 @@ def detalle_cliente(request, cliente_id):
         return JsonResponse(cliente)
     except Cliente.DoesNotExist:
         return JsonResponse({"error": "Cliente no encontrado"}, status=404)
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> 41263e665ae1dcc967eb641006b9b880fd8bc7eb
